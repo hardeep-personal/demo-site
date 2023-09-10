ec22a9547f7c71de26c1413de9c61356cfcfe9e0
Hardeep <hardeepde27@gmail.com>
2023-09-10 23:11:46 +0530

    test

---
+++ src/components/Video.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+const Video = () => {
+    return (
+        <>
+        
+            <iframe class="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+
+        </>
+    )
+}
+
+export default Video
